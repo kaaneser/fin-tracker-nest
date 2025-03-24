991be3ad3be19898ded8bbcfb8c2c6ee599b5944
Kaan Eser <69899907+kaaneser@users.noreply.github.com>
2025-03-24 18:04:25 +0300

    FIN-2: Recurring Transactions Implementation - Implemented new fields for the schema and dto for recurring transactions - Implemented calculations of next execution date & called that from create and update endpoints

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -4,7 +4,7 @@ import { Model } from "mongoose";
 import { Transaction } from "./schema/transaction.schema";
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { Category } from 'src/category/schema/category.schema';
-
+import * as moment from 'moment';
 
 @Injectable()
 export class TransactionsService {
@@ -13,11 +13,32 @@ export class TransactionsService {
     @InjectModel("Category") private categoryModel: Model<Category>
   ) {}
 
+  private async calculateNextExecDate(date: Date, frequency: "daily" | "weekly" | "monthly" | "yearly"): Promise<Date> {
+    switch (frequency) {
+      case "daily":
+        return moment(date).add(1, "days").toDate();
+      case "weekly":
+        return moment(date).add(1, "weeks").toDate();
+      case "monthly":
+        return moment(date).add(1, "months").toDate();
+      case "yearly":
+        return moment(date).add(1, "years").toDate();
+      default:
+        throw new Error("Invalid frequency type");
+    }
+  }
+
   async createTransaction(createTransactionDto: CreateTransactionDto, userId: string): Promise<Transaction> {
-    const transaction = new this.transactionModel({
+    const transactionData: any = {
       ...createTransactionDto,
       userId
-    });
+    };
+
+    if (createTransactionDto.isRecurring && createTransactionDto.frequency) {
+      transactionData.nextExecDate = await this.calculateNextExecDate(new Date(), createTransactionDto.frequency);
+    }
+    
+    const transaction = new this.transactionModel(transactionData);
 
     return transaction.save();
   }
@@ -55,6 +76,10 @@ export class TransactionsService {
   }
 
   async updateTransactionById(id: string, updateTransactionDto: CreateTransactionDto, userId: string) {
+    if (updateTransactionDto.isRecurring && updateTransactionDto.frequency) {
+      updateTransactionDto.nextExecDate = await this.calculateNextExecDate(new Date(), updateTransactionDto.frequency);
+    }
+    
     const updatedTransaction = await this.transactionModel.findOneAndUpdate(
       { userId, _id: id },
       { $set: updateTransactionDto },
